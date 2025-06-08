a623f214fb5ad94cf8ec72d2b532a543afc901f3
Aiman-0410 <aimanmakandar20@gmail.com>
2025-06-08 14:04:15 +0530

    Add files via upload

---
+++ numbers.php
@@ -0,0 +1,15 @@
+<?php
+// Initialize variables
+$value1 = 443.3432;
+$value2 = 7543.829;
+
+// Display the ceiling value of $value1
+echo "<p>Ceiling Value of $value1: " . ceil($value1) . "</p>";
+
+// Display the floor value of $value2
+echo "<p>Floor Value of $value2: " . floor($value2) . "</p>";
+
+// Generate a random number between 500 and 5000
+$randomNumber = rand(500, 5000);
+echo "<p>Random Number between 500 and 5000: $randomNumber</p>";
+?>
