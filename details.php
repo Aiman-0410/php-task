13be3c7dff8bae815204e865c6a48b6c20196e36
Aiman-0410 <aimanmakandar20@gmail.com>
2025-06-08 12:38:06 +0530

    Add files via upload

---
+++ details.php
@@ -0,0 +1,9 @@
+<?php
+// Create a new PHP file called details.php
+// Print name, age, and address using separate echo statements
+
+echo "Name: Umme Aiman<br>";
+echo "Age: 20<br>";
+echo "Address: Hubballi, Karnataka, India<br>";
+
+?>
