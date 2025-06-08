db8b27faa275bcf1b81e71ba71dc5d6ab3cc3ff1
Aiman-0410 <aimanmakandar20@gmail.com>
2025-06-08 12:50:36 +0530

    details.php

--- details.php
+++ details.php
@@ -1,9 +1,5 @@
 <?php
-// Create a new PHP file called details.php
-// Print name, age, and address using separate echo statements
-
 echo "Name: Umme Aiman<br>";
 echo "Age: 20<br>";
 echo "Address: Hubballi, Karnataka, India<br>";
-
 ?>
